112b46e46e16ddfa1c284e337a1bc56c613b377a
Gleb Dovzhenko <dovjenko.g@gmail.com>
2025-05-08 12:59:32 +0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "My raylib C to WASM compilation setup";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , ...
+    }:
+    let
+      overlays = [ ];
+      systems = [ "x86_64-linux" ];
+    in
+    flake-utils.lib.eachSystem systems (
+      system:
+      let
+        pkgs = import nixpkgs { inherit overlays system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+
+          nativeBuildInputs = with pkgs; [
+            libGL
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXinerama
+            xorg.libXi
+            libxkbcommon
+            #emscripten
+          ]
+          ++ [ ./myemsc.nix ];
+
+
+          shellHook = ''
+            cp -r ${pkgs.emscripten}/share/emscripten/cache ~/.emscripten_cache
+            chmod u+rwX -R ~/.emscripten_cache
+            export EM_CACHE=~/.emscripten_cache
+          '';
+
+        };
+
+        devShell = self.devShells.${system}.default;
+      }
+    );
+} 
---
+++ run.sh
@@ -0,0 +1,13 @@
+emcc -o index.html \
+    basic_window.c \
+    -lraylib -Os -Wall \
+    external/raylib-5.5/src/*.a \
+    -I external/raylib-5.5/src/ \
+    -L external/raylib-5.5/src/ \
+    -s USE_GLFW=3 \
+    -s ASYNCIFY \
+    --shell-file shell.html \
+    -s TOTAL_STACK=64MB \
+    -s INITIAL_MEMORY=128MB \
+    -s ASSERTIONS \
+    -DPLATFORM_WEB 
